f999dd9d62c6a9ad21fb36ec0cc13c15a1e6b3d0
Pablo Roulet <pablo.roulet@possible.com>
2019-06-18 14:34:11 -0300

    Initial Commit

---
+++ MeetingNotifier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MeetingNotifier
+//
+//  Created by Pablo Roulet on 18/06/2019.
+//  Copyright © 2019 Pablo Roulet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
